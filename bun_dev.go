9d3e1cdecb6f220b30ce81ceba715a42fccb6969
Alexander Vasilev <avasilev@abtronics.ru>
2025-04-09 01:19:02 +0300

    remove debug parameter

--- bun_dev.go
+++ bun_dev.go
@@ -22,7 +22,7 @@ func (s *NodePMProxy) runBunDev() {
 	if s.Framework == SVELTE {
 		cmd = exec.Command("bun", "dev", "--port", fmt.Sprintf("%d", s.Port))
 	} else {
-		cmd = exec.Command("bun", "dev", "qqq")
+		cmd = exec.Command("bun", "dev")
 	}
 
 	cmd.Stdout = os.Stdout
